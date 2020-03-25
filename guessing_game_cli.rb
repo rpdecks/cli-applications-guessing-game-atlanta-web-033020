35a7812ba321756b34e0afbae1de009dd433bb41
Robert Phillips <rpdecks@gmail.com>
2020-03-25 03:13:31 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 def run_guessing_game
   my_num = rand(6) + 1
-  my_num.to_f
   puts "Guess a number between 1 and 6."
   guess = gets.chomp
-  
+  if guess == my_num.to_s
+    
 end
